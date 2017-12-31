2097feb798af0e56e736fbbaec42121bde334b93
sambath <sambathkumar.rsk@gmail.com>
2017-12-31 17:07:58 -0600

    first

---
+++ src/main/java/micro/spring/securerui/microspringsecureui/MicroSpringSecureuiApplication.java
@@ -0,0 +1,12 @@
+package micro.spring.securerui.microspringsecureui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSpringSecureuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSpringSecureuiApplication.class, args);
+	}
+}
---
+++ src/test/java/micro/spring/securerui/microspringsecureui/MicroSpringSecureuiApplicationTests.java
@@ -0,0 +1,16 @@
+package micro.spring.securerui.microspringsecureui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroSpringSecureuiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
