55225618d81a62e4b8bc8c1114c92841da543ea0
sambath <sambathkumar.rsk@gmail.com>
2018-01-04 00:43:01 -0600

    working version

---
+++ src/main/java/micro/spring/securerui/microspringsecureui/ReportController.java
@@ -0,0 +1,32 @@
+package micro.spring.securerui.microspringsecureui;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableOAuth2Sso
+public class ReportController extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/")
+	public String loadHome() {
+		
+		return "Home" ;
+		
+	}
+	
+	
+	@Override
+	protected void configure (HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeRequests()
+		.antMatchers("/", "/login")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
+	}
+	
+}
